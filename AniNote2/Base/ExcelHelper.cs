995ea0e0a408b12d31893bff17567f7ea15c4897
Kaze <Andreas.Keimel@hotmail.de>
2023-07-25 18:14:26 +0200

    Impl: Addition for faster excel copy

---
+++ AniNote2/Base/ExcelHelper.cs
@@ -0,0 +1,18 @@
+﻿using AniNote2.MVM.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AniNote2.Base
+{
+    public static class ExcelHelper
+    {
+        public static string SingleEntry(AnimeItem animeItem)
+        {
+            string singleLine = $"{animeItem.Title}\t{animeItem.Episodes}";
+            return singleLine;
+        }
+    }
+}
